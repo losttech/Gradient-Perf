998e4bdbeab240e94c88a3e6f2f24595a9d44e58
Victor Milovanov <lost@losttech.software>
2020-03-19 16:58:08 -0700

    stub solution

---
+++ src/Bench.Gradient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bench.Gradient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
